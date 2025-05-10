8782652bb11a0fb7e45dac43d0b4bb7d4f5ad0d6
SCX-Quantum <scxquantum@gmail.com>
2025-05-10 15:04:26 -0300

    Add files via upload
    Um Script que lê dois números e mostra a soma entre eles.

---
+++ Desafio 4.py
@@ -0,0 +1,5 @@
+print(8*'=','MACHINE LEARNING DE SOMAR 2 NÚMEROS',8*'=')
+n1 = int(input('Digite um valor = '))
+n2 = int(input('Ótimo... \nAgora outro valor só pra eu testar uma coisa = '))
+s = n1 + n2
+print('Compreendo... {} + {} , se não estou enganado é {} ...\nEstou Certo?'.format(n1, n2, s))
