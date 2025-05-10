d3fc219611caf6eace02dfa52fc9780b2e94f667
SCX-Quantum <scxquantum@gmail.com>
2025-05-09 23:58:46 -0300

    Add files via upload
    Um script que recebe um NOME e imprime uma mensagem de boas vindas personalizada.

---
+++ Desafio 2.py
@@ -0,0 +1,6 @@
+print (10*"=", ' Olá Everybody ',10*'=')
+print ('')
+nome = input('Qual seria o seu belíssimo nome?')
+print ('Seja muito bem vindo {}, espero que \nencontre o que procura nesse diretório.'.format(nome))
+print ('Até a próxima (y)')
+
