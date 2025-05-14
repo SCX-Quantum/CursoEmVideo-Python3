1070f8f91a2040a9e315a12658f49ae92e28a171
SCX-Quantum <scxquantum@gmail.com>
2025-05-14 14:10:41 -0300

    Add files via upload
    Script que lê um número real e mostra sua porção inteira.

---
+++ Desafio 16.py
@@ -0,0 +1,9 @@
+from math import trunc
+
+print(30*'=')
+print('       PORÇÕES INTEIRAS')
+print(30*'=')
+print('')
+num = float(input('Digite um numero real:'))
+print('')
+print(f'A parte inteira do seu número é: {trunc(num)}')
